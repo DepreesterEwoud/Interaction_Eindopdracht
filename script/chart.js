7ae692bc62a62340f0e5f59dbaa69095c89ec1c2
Ewoud <ewoud.de.preester@student.howest.be>
2020-12-12 17:36:29 +0100

    basis

---
+++ script/chart.js
@@ -0,0 +1,191 @@
+let color;
+let html_graph = '.js-graph';
+let ctx;
+var myChart;
+const loadDataChart = async function (cryptocurrency, days, token) {
+
+    let ArrayXValues = [];
+    let ArrayYValues = [];
+
+    const urlHighMaxDaily = `https://min-api.cryptocompare.com/data/v2/histoday?fsym=${cryptocurrency}&tsym=EUR&limit=${days}`;
+    const response = await fetch(urlHighMaxDaily, {
+        headers: head
+        
+    });
+    
+    const data = await response.json();
+
+    for (let dataItem of data.Data.Data) {
+        let unixTime = dataItem.time
+        let high = dataItem.high
+
+        dateObj = new Date(unixTime * 1000);
+
+        dayNumber = dateObj.getDate();
+        nameDay = days[dateObj.getDay()];
+        nameMonth = months[dateObj.getMonth()];
+        numberMonth = dateObj.getMonth()+1;
+        //console.log(dateObj.getMonth()+1);
+
+        let stringTime = dayNumber + "/" + numberMonth;
+
+        ArrayXValues.push(stringTime);
+        ArrayYValues.push(high);
+    }
+
+    showChart(ArrayXValues, ArrayYValues, token);
+}
+
+
+
+const showChart = function (xValues, yValues, token) {
+
+    
+    
+    var json = `{"jsonarray": [`;
+    let i = 0;
+
+    for (let value of xValues) {
+        json += `{"Date": "${xValues[i]}",
+                  "Value": "${yValues[i]}"},`
+        i++;
+    }
+
+    var cuttedjson = json.substring(0, json.length - 1);
+
+    cuttedjson += "]}"
+
+    var jsonData = JSON.parse(cuttedjson);
+
+    var labels = jsonData.jsonarray.map(function (e) {
+        return e.Date;
+    });
+    var data = jsonData.jsonarray.map(function (e) {
+        
+        return e.Value;
+    });
+    
+    let pointOnly = [];
+    for (const point of data) {
+        pointOnly.push(null);
+    }
+
+    pointOnly.shift();
+    pointOnly.push(data[data.length - 1])
+
+    let red = "rgba(186, 70, 63, 0.8)";
+    let green = "rgba(74, 179, 97, 0.8)";
+
+    if (token == "-") {
+        color = red;
+
+
+    } else {
+        color = green;
+    }
+
+    console.log(labels);
+    
+    if(document.querySelector(".js-graph")){
+        html_graph.innerHTML = `<canvas id="myChart" style="width:10px;height:100%;></canvas>`;
+        ctx = document.querySelector('#myChart').getContext('2d');
+        var gradientFill = ctx.createLinearGradient(0, 0, 0, 800);
+        gradientFill.addColorStop(0, color);
+        gradientFill.addColorStop(1, "transparent");
+
+        let config = {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    data: data,
+                    backgroundColor: gradientFill,
+                    pointRadius: 0
+                },],
+            },
+            options: {
+                responsive: true,
+                legend: {
+                    display: false
+                },
+            
+
+                scales: {
+                    xAxes: [{
+                        gridLines: {
+                            color: "rgba(0, 0, 0, 0)",
+                        },
+                        /* scaleLabel: {
+                            display: true,
+                            labelString: 'Date',
+            
+                          }, */
+                        ticks: {
+                            fontColor: "black"
+                        }
+                    }],
+                    yAxes: [{
+                        gridLines: {
+                            color: "rgba(0, 0, 0, 0)",
+                        },
+
+                        ticks: {
+                            fontColor: "black"
+
+                        },
+                    },],
+                },
+            },
+        };
+        
+        myChart = new Chart(ctx, config);
+        myChart.update();
+        
+    };
+    /* new Chart(ctx, {
+        //animationEnabled: true,
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                data: data,
+                backgroundColor: gradientFill,
+                pointRadius: 0
+            },{
+                data: pointOnly,
+                borderWidth: 8,
+                pointHitRadius: 5,
+
+            }]
+        },
+        options: {
+            responsive: true,
+            legend: {
+                display: false
+            },
+           
+
+            scales: {
+                xAxes: [{
+                    gridLines: {
+                        color: "rgba(0, 0, 0, 0)",
+                    },
+                    ticks: {
+                        fontColor: "black"
+                    }
+                }],
+                yAxes: [{
+                    gridLines: {
+                        color: "rgba(0, 0, 0, 0)",
+                    },
+                    ticks: {
+                        fontColor: "black"
+
+                    }
+                }]
+            },
+        }
+    }); */
+
+
+}
---
+++ script/main.js
@@ -0,0 +1,268 @@
+// ----------------------------------- VARS, LETS  ----------------------------------- //
+let domSelect, domPercentage, domDiffrence, domVolume, domUpdated, domLow, domHigh, domButtons, amount, domLogo = "";
+
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const head = {
+    "authorization": "Apikey 919252046791580f51b013cdbc726e4cef0aedc6de9585221c0e721df92fe1de"
+}
+
+let selectedValue = "BTC";
+
+let dayscount = 7;
+
+let symbol = "";
+
+let valueYesterday, valueNow, percentage, volume = 0;
+
+
+
+// ----------------------------------- BASICS  ----------------------------------- //
+
+const getDOM = function () {
+    domSelect = document.querySelector('.js-select');
+    domPercentage = document.querySelector('.js-percentage');
+    domVolume = document.querySelector('.js-volume');
+    domUpdated = document.querySelector('.js-updated');
+    domLow = document.querySelector('.js-low');
+    domHigh = document.querySelector('.js-high');
+    domButtons = document.querySelectorAll(".js-button");
+    domLogo = document.querySelector('.js-logo');
+    domDiffrence = document.querySelector('.js-diffrence');
+}
+
+const enableListeners = function () {
+    domSelect.addEventListener('change', function () {
+        document.querySelector('.c-grid').style.display='none';
+        document.getElementById('load').classList.add('loader');
+        setTimeout(()=>{
+            document.getElementById('load').classList.remove('loader');
+            document.querySelector('.c-grid').style.display='grid';
+            
+        },2000)
+        
+        selectedValue = this.value; 
+        showData(selectedValue);
+        
+        if(selectedValue == "XRP")
+        {
+            domLogo.src = "https://static.cryptotips.eu/wp-content/uploads/2018/01/ripple-xrp-logo.png";
+            document.body.style.backgroundColor = 'rgba(14, 155, 210, .3)';
+            
+
+        }
+
+        if(selectedValue == "BTC")
+        {
+            domLogo.src = "https://btcdirect.eu/media/1108/download/Bitcoin.png?v=1";
+            document.body.style.backgroundColor = 'rgba(255, 179, 71, .6)';
+            
+        }
+
+        if(selectedValue == "ETC")
+        {
+            domLogo.src = "https://static.cryptotips.eu/wp-content/uploads/2018/05/ethereum-classic-etc-logo-230x230.png";
+            document.body.style.backgroundColor = 'rgba(57, 183, 57, .3)';
+        }
+
+        if(selectedValue == "ETH")
+        {
+            domLogo.src = "https://etherscan.io/images/ethereum-icon.png";
+            document.body.style.backgroundColor = 'rgba(168, 185, 198, .3)';
+        }
+        
+        if(selectedValue == "BAT")
+        {
+            domLogo.src = "https://cdn.worldvectorlogo.com/logos/basic-attention-token.svg";
+            document.body.style.backgroundColor = 'rgba(255, 80, 0, .3)';
+        }
+
+    })
+
+
+
+    for (let button of domButtons) {
+        button.classList.remove('is-selected');
+        button.addEventListener('click', function () {
+
+            buttons = document.getElementsByClassName('c-link-cta');
+            for(let clink of buttons)
+            {
+                clink.classList.remove('is-selected');
+            }
+
+
+            dayscount = button.getAttribute('data-value');
+            loadDataChart(selectedValue, dayscount, symbol);
+            button.childNodes[1].classList.add('is-selected');
+        })
+    }
+
+}
+
+const init = function () {
+    document.querySelector('.c-grid').style.display='none';
+    document.getElementById('load').classList.add('loader');
+    setTimeout(()=>{
+        document.getElementById('load').classList.remove('loader');
+        document.querySelector('.c-grid').style.display='grid';
+        
+    },2000)
+    getDOM();
+    enableListeners();
+    showData("BTC");
+    domLogo.src = "https://btcdirect.eu/media/1108/download/Bitcoin.png?v=1";
+    document.body.style.backgroundColor = 'rgba(255, 179, 71, .6)';
+}
+
+
+// ----------------------------------- SHOWING DATA  ----------------------------------- //
+
+
+const showData = function (cryptocurrency) {
+    
+    loadPrices(cryptocurrency);
+    loadDiffrence(cryptocurrency);
+    loadDataUpdated(cryptocurrency);
+    loadPricesHighLow(cryptocurrency);
+}
+
+const showPrices = function () {
+    symbol = "";
+    percentage = (1 - (valueYesterday / valueNow)) * 100;
+
+    domHolder = document.querySelector(`.js-price`)
+    domHolder.innerHTML = "€" + valueNow;
+    //countUp(valueYesterday, valueNow, "js-price");
+
+    if (percentage < 0) {
+        symbol = "-";
+        domPercentage.setAttribute("style", "color: var(--global-color-rood)")
+    } else {
+        symbol = "+";
+        domPercentage.setAttribute("style", "color: var(--global-color-green)")
+    }
+
+
+    domPercentage.innerHTML = symbol + " " + Math.abs(percentage.toFixed(2)) + " %";
+
+    loadDataChart(selectedValue, dayscount, symbol);
+}
+
+
+const showLastUpdated = function (data, cryptocurrency) {
+    let unixTime = data.RAW[cryptocurrency].EUR.LASTUPDATE
+
+    dateObj = new Date(unixTime * 1000);
+
+
+    dayNumber = dateObj.getDate();
+    nameDay = days[dateObj.getDay()];
+    nameMonth = months[dateObj.getMonth()];
+    houre = dateObj.getHours();
+    minutes = dateObj.getMinutes();
+    seconds = dateObj.getSeconds();
+
+    let stringUpdated = dayNumber + " " + nameMonth + " " + houre + ":"+ minutes+":"+seconds;
+
+    domUpdated.innerHTML = stringUpdated;
+}
+
+const showRangeDaily = function (min, max) {
+    domHigh.innerHTML = max.replace(",", " ")
+    domLow.innerHTML = min.replace("," , " ")
+}
+
+const showDiffrence = function (min,max) {
+    symbol = "";
+    amount = max - min;
+    console.log(amount);
+
+
+    if (amount < 0) {
+        symbol = "-";
+        domDiffrence.setAttribute("style", "color: var(--global-color-rood)")
+    } else {
+        symbol = "+";
+        domDiffrence.setAttribute("style", "color: var(--global-color-green)")
+    }
+
+
+    domDiffrence.innerHTML = symbol + " €" + Math.abs(amount.toFixed(2));
+
+
+}
+
+
+
+// ----------------------------------- LOADING DATA  ----------------------------------- //
+
+
+const loadPrices = function (cryptocurrency) {
+    const urlAPI = `https://min-api.cryptocompare.com/data/price?fsym=${cryptocurrency}&tsyms=EUR`;
+
+    fetch(urlAPI, {
+        headers: head
+    }).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        valueNow = data.EUR;
+        loadHighLowPrices(cryptocurrency);
+    });
+
+}
+
+const loadHighLowPrices = function (cryptocurrency) {
+    const urlAPI = `https://min-api.cryptocompare.com/data/v2/histoday?fsym=${cryptocurrency}&tsym=EUR&limit=10`;
+
+    fetch(urlAPI, {
+        headers: head
+    }).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        valueYesterday = data.Data.Data[9].open;
+        showPrices();
+    });
+}
+
+const loadDiffrence = function (cryptocurrency) {
+    const urlAPI = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cryptocurrency}&tsyms=EUR`;
+
+    fetch(urlAPI, {
+        headers: head
+    }).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        showDiffrence(data.RAW[cryptocurrency].EUR.LOW24HOUR, data.RAW[cryptocurrency].EUR.HIGH24HOUR)
+    });
+}
+
+const loadDataUpdated = function (cryptocurrency) {
+    const urlAPI = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cryptocurrency}&tsyms=EUR`;
+
+    fetch(urlAPI, {
+        headers: head
+    }).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        showLastUpdated(data, cryptocurrency);
+    });
+}
+
+const loadPricesHighLow = async function (cryptocurrency) {
+    const urlAPI = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cryptocurrency}&tsyms=EUR`;
+    
+    fetch(urlAPI, {
+        headers: head
+    }).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        
+        showRangeDaily(data.DISPLAY[cryptocurrency].EUR.LOW24HOUR, data.DISPLAY[cryptocurrency].EUR.HIGH24HOUR);
+    });
+}
+
+
+
+document.addEventListener('DOMContentLoaded', init)
